4cb5f1b88ce109832a89c6aecdef45f7abe72b50
Andre Rodriguez <drae7299@gmail.com>
2021-06-19 23:29:23 -0400

    more scss work

--- reactportfolio/src/components/topbar/Topbar.jsx
+++ reactportfolio/src/components/topbar/Topbar.jsx
@@ -3,7 +3,10 @@ import "./topbar.scss"
 export default function Topbar() {
     return (
         <div className ="topbar">
-            
+            <div className="left">
+                <a href="#intro"> testing</a>
+            </div>
+            <div className ="right"></div>
         </div>
     )
 }
