b7aed2a042448f288f6a752b979ff8458f31f308
Andre Rodriguez <drae7299@gmail.com>
2021-06-20 02:52:04 -0400

    finished main UI and dynamic interaction for main page

--- reactportfolio/src/components/topbar/Topbar.jsx
+++ reactportfolio/src/components/topbar/Topbar.jsx
@@ -2,32 +2,31 @@ import "./topbar.scss";
 import EmailIcon from "@material-ui/icons/Email";
 import PersonIcon from "@material-ui/icons/Person";
 
-
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className= "logo">
-          andre rodriguez.
+          <a href="#intro" className="logo">
+            andre rodriguez.
           </a>
           <div className="itemContainer">
-          <PersonIcon className="icon" />
-          <span id="biggy">+ 44 924 22 </span>
+            <PersonIcon className="icon" />
+            <span>614-592-8914 </span>
           </div>
           <div className="itemContainer">
-          <EmailIcon className="icon" /> 
-          <span id="biggy">safe@dre.com</span>
+            <EmailIcon className="icon" />
+            <span>drae7299@gmail.com</span>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
-
-
-
-
-
