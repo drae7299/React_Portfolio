d93fa6b59eb14ab9f9ec3919a959b7ba3166b528
Andre Rodriguez <drae7299@gmail.com>
2021-06-20 01:13:13 -0400

    corrected issues with material icons module

--- reactportfolio/src/components/topbar/Topbar.jsx
+++ reactportfolio/src/components/topbar/Topbar.jsx
@@ -1,14 +1,22 @@
 import "./topbar.scss";
+import EmailIcon from "@material-ui/icons/Email";
+import PersonIcon from "@material-ui/icons/Person";
 
-export default function Topbar() {
+
+function Topbar() {
   return (
     <div className="topbar">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className= "logo">andre rodriguez.</a>
+          <a href="#intro" className= "logo">andre rodriguez.
+          </a>
           <div className="itemContainer">
-          
-
+          <PersonIcon className="icon"/>
+          <span>+ 44 924 22 </span>
+          </div>
+          <div className="itemContainer">
+          <EmailIcon className="icon"/> 
+          <span>safe@dre.com</span>
           </div>
         </div>
         <div className="right"></div>
@@ -16,3 +24,5 @@ export default function Topbar() {
     </div>
   );
 }
+
+export default Topbar;
